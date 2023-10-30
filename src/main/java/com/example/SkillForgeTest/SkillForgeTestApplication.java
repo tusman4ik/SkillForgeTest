d7bd4c0c1464b880b463ddae0860f3333c9fa431
Tusman4ik <tusman4ik@gmail.com>
2023-10-30 20:59:52 +0300

    Initial commit

---
+++ src/main/java/com/example/SkillForgeTest/SkillForgeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkillForgeTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillForgeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillForgeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SkillForgeTest/SkillForgeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkillForgeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillForgeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
