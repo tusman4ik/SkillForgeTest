913e0b5624af9c16949603abebcea0f67ca41b1e
ADTOP4IK <contact@adaeff.ru>
2023-11-08 18:08:57 +0300

    Сделал страницу адаптивнее, подключил jquery и модальные окна, изменил стили с px на vh wv em rem, сделал инклуды, изменил цвет плеера, изменил шапку.

--- src/main/resources/static/js/includes.js
+++ src/main/resources/static/js/includes.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-  $("#info").load("./includes/info.txt");
+  $("#info").load("./includes/info.html");
 });
 
 $(document).ready(function () {
   $("#textteory").load("./includes/text-teory.txt");
+});
+
+$(document).ready(function () {
+  $("#taskinfo").load("./includes/taskinfo.html");
 });
